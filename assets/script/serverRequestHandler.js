be048b8c12a01a3cbad234c2a0956f148531ae15
yuvarajucet <yuvarajucet@gmail.com>
2023-04-23 19:16:51 +0530

    complete additem

--- assets/script/serverRequestHandler.js
+++ assets/script/serverRequestHandler.js
@@ -1,6 +1,10 @@
+export const host = Object.freeze({
+    HOST:"http://127.0.0.1:8000"
+});
+
 // inital amount realted endpoints
 export const initialAmountEndPoint = Object.freeze({
-    addItem: '',
+    addItem: '/api/v1/addamount',
     removeItem: '',
 });
 
@@ -13,19 +17,39 @@ export const spentAmountEndPoint = Object.freeze({
 
 
 export function MakeServerRequest(initialAmount){
-    var inputData = { "userInitialAmount": initialAmount };
+    var inputData = { "amount": initialAmount };
     $.ajax({
         type: "POST",
-        url: initialAmount.addItem,
+        url: host.HOST+initialAmountEndPoint.addItem,
         data: JSON.stringify(inputData),
         headers : {
-            "Content-Type":"application/json",
+            "Content-Type":"application/json"
         },
         success: function(response){
-            debugger;
+            if (response.status_code == 200){
+                debugger;
+                $("#initialAmt")[0].value = "";
+                addInitialDataIntoTable(JSON.parse(response.data));
+            } else{
+                // var target = $(".error");
+                // target.append($("<p>" + response.message + "</p>"));
+            }
         },
         error: function(response){
-            debugger;
+            console.log(response);
         }
     });
+}
+
+
+function addInitialDataIntoTable(data){
+    debugger;
+    var target = $("#initialtable");
+    if (target.children().length > 0){
+        target.children().remove();
+    }
+    var tableHeader = $("<tr><th>S.No</th><th>Initial Amount</th><th>Edit</th></tr>");
+    var tableData = $("<tr><td>1</td><td> &#8377; " + data.amount + "</td><td>Edit</td></tr>");
+    target.append(tableHeader);
+    target.append(tableData);
 }
